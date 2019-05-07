63630c8bd8051f5506a19c1e358ac60972e402c6
jon <43426006+jcwalp@users.noreply.github.com>
2019-05-07 14:14:58 -0400

    v1.84

--- login.js
+++ login.js
@@ -1,4 +1,14 @@
 
-function validate(){
-  
+function verify(){
+    var un = document.loginform.email.value;
+    var pw = document.loginform.password.value;
+    var username = "jwalp@sjci.com";
+    var password = "password";
+    if ((un == username) && (pw == password)) {
+        return true;
+    }
+    else {
+        alert ("Login was unsuccessful, please check your username and password");
+        return false;
+    }
 }
