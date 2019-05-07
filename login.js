8cd36474f4f204a53dbc273d7e4ea2e875a595e2
jon <43426006+jcwalp@users.noreply.github.com>
2019-05-07 13:45:52 -0400

    v1.83

--- login.js
+++ login.js
@@ -1,10 +1,4 @@
-var usernameValid = "kdluhy@sjci.com";
-var passwordValid = "password123";
 
 function validate(){
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  if (username == usernameValid && password == passwordValid){
-    alert("Correct");
-  }
+  
 }
